a087ad566ac028dc4f47c8d8c0b2393fde9360a1
Thomas Duchatelet <thomas.duchatelet.y3284@student.hogent.be>
2019-12-26 15:57:21 +0100

    Added tests

---
+++ SportsStoreEF.Tests/Models/CategoryTest.cs
@@ -0,0 +1,66 @@
+﻿using SportsStore.Models;
+using Xunit;
+
+namespace SportsStore.Tests.Models
+{
+    public class CategoryTest
+    {
+        private Category _category;
+        private Product _football;
+
+        public CategoryTest()
+        {
+            _category = new Category("Soccer");
+            _football = new Product("Football", 10);
+        }
+
+        [Fact]
+        public void Category_StartsEmpty()
+        {
+            Assert.Equal(0, _category.Products.Count);
+        }
+
+        [Fact]
+        public void Add_NewProduct_AddsProduct()
+        {
+            _category.AddProduct("Football", 10, null);
+            Assert.Equal(1, _category.Products.Count);
+        }
+
+        [Fact]
+        public void Add_ProductNotInCategory_AddsProduct()
+        {
+            _category.AddProduct(_football);
+            Assert.Equal(1, _category.Products.Count);
+        }
+
+        [Fact]
+        public void Add_ProductInCategory_DoesnotAddProduct()
+        {
+            _category.AddProduct(_football);
+            _category.AddProduct(_football);
+            Assert.Equal(1, _category.Products.Count);
+        }
+
+        [Fact]
+        public void Remove_RemovesProduct()
+        {
+            _category.AddProduct(_football);
+            _category.RemoveProduct(_football);
+            Assert.Equal(0, _category.Products.Count);
+        }
+
+        [Fact]
+        public void FindProduct_ProductInCategory_ReturnsProduct()
+        {
+            _category.AddProduct("Football", 10, null);
+            Assert.NotNull(_category.FindProduct("Football"));
+        }
+
+        [Fact]
+        public void FindProduct_ProductNotInCategory_ReturnsNull()
+        {
+            Assert.Null(_category.FindProduct("Football"));
+        }
+    }
+}
---
+++ SportsStoreEF.Tests/Models/OrderTest.cs
@@ -0,0 +1,40 @@
+﻿using SportsStore.Models;
+using Xunit;
+
+namespace SportsStore.Tests.Models
+{
+    public class OrderTest
+    {
+        private readonly Order _order;
+        private readonly Product _p1;
+        private readonly Product _p3;
+
+        public OrderTest()        {
+            Cart cart = new Cart();
+            _p1 = new Product( "Football", 10, null,1);
+            Product p2 = new Product( "Short", 5, null, 2);
+            _p3 = new Product (  "NotInOrder",  5, null, 3);
+            cart.AddLine(_p1, 1);
+            cart.AddLine(p2, 10);
+            _order = new Order(cart, null, false, "Teststraat 10", new City("9000", "Gent"));
+        }
+
+        [Fact]
+        public void Total_ReturnsSumOfOrderlines()
+        {
+               Assert.Equal(60, _order.Total);
+        }
+        [Fact]
+        public void HasOrdered_ProductInOrder_ReturnsTrue()
+        {
+            Assert.True(_order.HasOrdered(_p1));
+        }
+
+        [Fact]
+        public void HasOrdered_ProductNotInOrder_ReturnsFalse()
+        {
+            Assert.False(_order.HasOrdered(_p3));
+        }
+     
+    }
+}
